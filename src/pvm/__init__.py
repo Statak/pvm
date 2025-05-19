1a792feae51332267ca13f7334e9d3fecdf03118
Dazzy Mlv <23006212+DazzyMlv@users.noreply.github.com>
2025-05-19 20:18:57 +0530

    fix(version): revert version to 0.1.0 in pyproject.toml and __init__.py

--- src/pvm/__init__.py
+++ src/pvm/__init__.py
@@ -1,7 +1,1 @@
-from pathlib import Path
-
-from pvm.pyproject import PyProject
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-__version__ = PyProject(BASE_DIR / 'pyproject.toml').version
+__version__ = "0.1.0"  # Version Var
